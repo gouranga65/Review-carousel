b714a7a518a1716ac9ab8a9329a7a592959aa8b0
sundar <gm49302@gmail.com>
2022-11-05 23:22:16 +0530

    logo add+ js add

---
+++ app.js
@@ -0,0 +1,8 @@
+let review = {
+  reviewOne:
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
+  reviewTwo:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  reviewThree:
+    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
+};
