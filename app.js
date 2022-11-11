55cd133ff4109d02bb6ebcf3849b0e6088e1fc2c
sundar <gm49302@gmail.com>
2022-11-11 12:09:32 +0530

    cant understand

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-let review = {
-  reviewOne:
-    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form",
-  reviewTwo:
-    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-  reviewThree:
-    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old",
-};
+// select items
+let img = document.getElementById("person-img");
+let author = document.getElementById("author");
+let job = document.getElementById("job");
+let info = document.getElementById("info");
+
+let prevBtn = document.querySelector(".prev-btn");
+let nextBtn = document.querySelector(".next-btn");
+let randomBtn = document.querySelector(".random-btn");
+
+// set current item
+let currentItem=0;
