e8b22f4074463f541d67616a9a3f8d4c20624ade
sundar <gm49302@gmail.com>
2023-01-02 22:33:22 +0530

    random button done till now

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
-// select items
-let img = document.getElementById("person-img");
-let author = document.getElementById("author");
-let job = document.getElementById("job");
-let info = document.getElementById("info");
-
-let prevBtn = document.querySelector(".prev-btn");
-let nextBtn = document.querySelector(".next-btn");
-let randomBtn = document.querySelector(".random-btn");
-
-// set current item
-let currentItem=0;
+const naam = document.querySelector(".name");
+const kaam = document.querySelector(".position");
+const sameeksha = document.querySelector(".review");
+const leftsideBtn=document.querySelector(".leftsideBtn").addEventListener('click',function(){
+    for (let index = 0; index < arr.length; index++) {
+        if (index<index+1) {
+            kaam.innerHTML=arr[index].position;
+            index=index+1;
+            break;
+        }
+        
+    }
+})
+let arr = [
+  {
+    name: "gouranga",
+    position: "one",
+    review: "one",
+  },
+  {
+    name: "gouranga",
+    position: "two",
+    review: "two",
+  },
+  {
+    name: "gouranga",
+    position: "three",
+    review: "three",
+  },
+  {
+    name: "gouranga",
+    position: "four",
+    review: "four",
+  },
+  {
+    name: "gouranga",
+    position: "five",
+    review: "five",
+  },
+  {
+    name: "gouranga",
+    position: "six",
+    review: "six",
+  },
+];
+const random=document.querySelector(".random").addEventListener("click",function(){
+    var result=Math.floor(Math.random()*arr.length)
+    kaam.innerHTML=arr[result].position;
+});
