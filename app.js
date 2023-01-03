a091fcf8ec3726ae01d5a3c03825ac4dae299fe5
sundar <gm49302@gmail.com>
2023-01-03 23:29:03 +0530

    random (color+name+reviews) done

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
 const naam = document.querySelector(".name");
 const kaam = document.querySelector(".position");
 const sameeksha = document.querySelector(".review");
-const leftsideBtn=document.querySelector(".leftsideBtn").addEventListener('click',function(){
-    for (let index = 0; index < arr.length; index++) {
-        if (index<index+1) {
-            kaam.innerHTML=arr[index].position;
-            index=index+1;
-            break;
-        }
-        
-    }
-})
+const photo = document.querySelector(".image");
 let arr = [
   {
-    name: "gouranga",
-    position: "one",
-    review: "one",
+    name: "Kallirroi Samaras",
+    position: "Auto Mechanic",
+    review:
+      "This Coca-Cola works outstandingly well. It beautifully improves my basketball by a lot.",
   },
   {
-    name: "gouranga",
-    position: "two",
-    review: "two",
+    name: "Theodosia Spiros",
+    position: "Chemist",
+    review: "My baboon loves to play with it.",
   },
   {
-    name: "gouranga",
-    position: "three",
-    review: "three",
+    name: "Angeliki Papadopoulos",
+    position: "Real Estate Agent",
+    review: "this Coca-Cola is standard.",
   },
   {
-    name: "gouranga",
-    position: "four",
-    review: "four",
+    name: "Vlassis Michelakos",
+    position: "Dancer",
+    review: "I tried to hang it but got jelly bean all over it.",
   },
   {
-    name: "gouranga",
-    position: "five",
-    review: "five",
+    name: "Aliki Michelakos",
+    position: "Janitor",
+    review:
+      "one of my hobbies is sailing. and when i'm sailing this works great.",
   },
   {
-    name: "gouranga",
-    position: "six",
-    review: "six",
+    name: "Zinovia Megalos",
+    position: "Registered Nurse",
+    review: "I tried to cremate it but got Turkish Delight all over it.",
   },
 ];
-const random=document.querySelector(".random").addEventListener("click",function(){
-    var result=Math.floor(Math.random()*arr.length)
-    kaam.innerHTML=arr[result].position;
-});
+const random = document
+  .querySelector(".random")
+  .addEventListener("click", function () {
+    var result = Math.floor(Math.random() * arr.length);
+    kaam.innerHTML = arr[result].position;
+    naam.innerHTML = arr[result].name;
+    sameeksha.innerHTML = arr[result].review;
+    let hash = "#";
+    let number = "0123456789ABCDEF";
+    for (let index = 0; index < 6; index++) {
+      hash = hash + number[Math.floor(Math.random() * 16)];
+    }
+    photo.style.background = hash;
+  });
